1dd7ef7c3432904d28c104251a3f083eed6e5401
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-05-30 21:24:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,29 +62,66 @@ int main(){
   cout << "enter the number of elements you are inputting" << endl;
   cin >> numelements;
   cin.get();
-  cout << "please enter your elements below. press enter between each one" << endl;       
+  cout << "Type 1 below to enter elements maually and type 2 below to upload from a file" << endl;
+  cin >> option;
+  cin.get();
+     if(option == 1){
+	  //text input: reads numbers from user and adds to array   
+          cout << "please enter your elements below. press enter between each one" << endl;       
           for(int i = 0; i < numelements; i++){
                int input;
                cin >> input;
                cin.get();
                arr[i] = input;
-          }
-            
-          Node* rootP = NULL;
-          for(int a = 0; a < numelements; a++){
-            rootP = insert(rootP, arr[a]);
-          }
+          }  
+       } else if(option == 2){
+	  //file input: reads numbers from file and add   
+		  ifstream myFile;
+		  myFile.open("numbers.txt");
+		  int g = 0;
+		  while (g < numelements) {
+   		  myFile >> arr[g];
+   		  g++;
+		  }
+		  myFile.close();
+    }
   
-  int number;
-  cout << "enter number to search?" <<endl;
-  cin>>number;
-  if(Search(rootP,number) == true){
-    cout << "found" << endl;
-  }else{
-    cout << "not found" << endl;
-  }
-  
-  print(rootP, -10);
+    Node* rootP = NULL;
+    for(int a = 0; a < numelements; a++){
+        rootP = insert(rootP, arr[a]);
+       }
+     
+    int choice = 0;
+    bool tf = true;
+    while(tf == true){
+      cout << "What would you like to do? Type 1 to add, Type 2 to remove, Type 3 to search, Type 4 to print, Type 5 to quit" << endl;
+      cin >> choice;
+      cin.get();
+      if(choice == 5){
+          tf =false;
+      }else if(choice == 4){
+          print(rootP, -10);
+      }else if(choice == 3){
+          int number;
+          cout << "enter number to search?" <<endl;
+          cin>>number;
+          if(Search(rootP,number) == true){
+            cout << "found" << endl;
+          }else{
+            cout << "not found" << endl;
+          }
+      }else if(choice == 2){
+          cout << "N/A yet" << endl;
+      }
+      else if(choice == 1){
+          numelements++;
+          int toAdd;
+          cout << "what number do you want to add" << endl;
+          cin >> toAdd;
+          cin.get();
+          rootP = insert(rootP, toAdd);
+      }
+   
 }
 
 
